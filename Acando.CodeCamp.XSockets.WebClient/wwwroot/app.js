343588f06b428a6e31f36ac10e2787894e490cde
Jesper <jesper.astrom@connecta.com>
2015-08-20 23:09:45 +0200

    Added basic angular stuffs

---
+++ Acando.CodeCamp.XSockets.WebClient/wwwroot/app.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app', ['ngRoute'])
+        .config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/', {
+            templateUrl: 'home/index.html',
+            controller: 'HomeController',
+            controllerAs: 'vm'
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+  }]);
+
+}(angular));
---
+++ Acando.CodeCamp.XSockets.WebClient/wwwroot/home/homeController.js
@@ -0,0 +1,15 @@
+﻿(function (angular) {
+    'use strict';
+
+    // ReSharper disable once InconsistentNaming
+    function HomeController() {
+        var vm = this;
+    }
+
+    HomeController.$inject = [];
+
+    angular
+        .module('app')
+        .controller('HomeController', HomeController);
+
+}(angular));

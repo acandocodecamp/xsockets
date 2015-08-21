b3b2d85f23a1734324d0c28cd2fc49f683a0ca3b
Jesper <jesper.astrom@connecta.com>
2015-08-21 22:14:47 +0200

    Added components project

---
+++ Acando.CodeCamp.XSockets.Components/ReportsController.cs
@@ -0,0 +1,16 @@
+using System;
+using XSockets.Core.Common.Socket.Event.Interface;
+using XSockets.Core.XSocket;
+using XSockets.Core.XSocket.Helpers;
+
+namespace Acando.CodeCamp.XSockets
+{
+    public class ReportsController : XSocketController
+    {
+        public override void OnMessage(IMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            this.InvokeToAll(message);
+        }
+    }
+}

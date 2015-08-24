a33c9644924a3bec4fbec45ea1e9de823f26638e
Jesper <jesper.astrom@connecta.com>
2015-08-24 22:01:07 +0200

    Added configuration file for console host.

---
+++ Acando.CodeCamp.Realtime.ConsoleHost/ConsoleHostConfigurationSetting.cs
@@ -0,0 +1,11 @@
+﻿using XSockets.Core.Configuration;
+
+namespace Acando.CodeCamp.Realtime
+{
+    internal class ConsoleHostConfigurationSetting : ConfigurationSetting
+    {
+        public ConsoleHostConfigurationSetting() : base("ws://localhost:4502")
+        {
+        }
+    }
+}

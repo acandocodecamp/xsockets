2f475cb800ce49f6fda2460558a68a6a25b8a3f0
Jesper <jesper.astrom@connecta.com>
2015-08-25 23:14:51 +0200

    Added an OWIN host

---
+++ Acando.CodeCamp.Realtime.OwinHost/OwinHostConfigurationSetting.cs
@@ -0,0 +1,11 @@
+﻿using XSockets.Core.Configuration;
+
+namespace Acando.CodeCamp.Realtime
+{
+    public class ConsoleHostConfigurationSetting : ConfigurationSetting
+    {
+        public ConsoleHostConfigurationSetting() : base("ws://127.0.0.1:4503")
+        {
+        }
+    }
+}
---
+++ Acando.CodeCamp.Realtime.OwinHost/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using Acando.CodeCamp.Realtime;
+using Microsoft.Owin;
+using Owin;
+using XSockets.Owin.Host;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace Acando.CodeCamp.Realtime
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //StringBuilder sb = new StringBuilder(100);
+            //sb.AppendLine("Hi.");
+            //sb.AppendLine("");
+            //sb.AppendLine("");
+            //sb.AppendLine("Listening on endpoints:");
+            //sb.AppendLine("");
+
+            app.UseXSockets();
+            //using (var container = Composable.GetExport<IXSocketServerContainer>())
+            //{
+            //    container.OnStarted += (sender, args) =>
+            //    {
+
+            //    };
+            //    container.Start();
+
+            //    foreach (var server in container.Servers)
+            //    {
+            //        sb.AppendLine(server.ConfigurationSetting.Endpoint.ToString());
+            //    }
+            //}
+
+            //app.Run(context =>
+            //{
+            //    context.Response.ContentType = "text/plain";
+            //    return context.Response.WriteAsync(sb.ToString());
+            //});
+        }
+    }
+}
